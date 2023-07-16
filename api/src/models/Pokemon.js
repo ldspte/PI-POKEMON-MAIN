c88c3328223f342fc97fa356c290fb595426f7b4
ldspte <ldspte9807@gmail.com>
2023-07-16 00:06:44 -0500

    back_1

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,10 +3,45 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define('Pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    attack: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    defense: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  },
+  {sequelize, timestamps: false});
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,64 @@
 const { Router } = require('express');
+const {getAllInfo, getPokemonById, getPokemonByName, postPokemon} = require('../controllers/index');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 
+
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/', (req, res) => {
+    try {
+        const allPokemons = getAllInfo();
+        if (allPokemons.error) throw Error(allPokemons.error);
+        return res.status(200).json(allPokemons)
+
+    } catch (error) {
+        return res.status(404).send(error.message);
+    }
+})
+
+router.get('/', (req, res) => {
+    try {
+        const {name} = req.query;
+        const pokemonName = getPokemonByName(name);
+        if (pokemonName.error) throw Error(pokemonName.error);
+        return res.status(200).json(pokemonName);
+    } catch (error) {
+        return res.status(404).send(error.message);
+    }
+})
+
+router.get('/:idPokemon', (req, res) => {
+    try {
+        const {idPokemon} = req.params;
+        const pokemonId = getPokemonById(idPokemon);
+        if(pokemonId.error) throw Error(pokemonId.error);
+        return res.status(200).json(pokemonId);
+    } catch (error) {
+        return res.status(404).send(error.message);
+    }
+});
+
+router.post('/', (req, res) => {
+    try {
+        const {name, image, life, attack, defense, weight, height} = req.body;
+        if (!name || !image || !life || !attack || !defense || !weight || !height) throw Error ('Debes llenar todos los campos');
+        const newPokemon = postPokemon(name, image, life, attack, defense, weight, height)
+        return res.status(200).json(newPokemon);
+    } catch (error) {
+        return res.status(404).send(error.message);
+        
+    }
+})
+
+
+
 
 
 module.exports = router;
